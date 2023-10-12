da0d761a9f4537d4a8f01b24a6eaca6d8faa50ee
Disavowedd <128288879+Disavowedd@users.noreply.github.com>
2023-10-12 23:33:07 +0800

    Removed stupid ass stdio

--- main.java
+++ main.java
@@ -1,1 +1,18 @@
-#include <stdio.h>
+/*
+implementation for MCO1:
+Phase 1:
+Features:
+All EL1 Creatures (9 creatures)
+Inventory System
+Areas Functionality, but only including AREA 1
+Text Based output simulation of the features
+*/
+
+
+
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
