fe3c5be6e04f4e0d943557635140c7f0fc4a266a
Aaron Abaniel <140327394+aabaniel@users.noreply.github.com>
2023-10-14 23:08:49 +0800

    Update Area.java
    Added some shit i forgor

--- Area.java
+++ Area.java
@@ -1,1 +1,21 @@
+/*
+Create area class here
+  */
+public class area{
+    private Integer aType;
+    private Integer areaGrid[][];
+    private Integer userGrid[][];
 
+    void initializeareaGrid()
+    {
+        areaGrid = new Integer[5][1]; 
+    }
+    void initializeBattle()
+    {
+        //do something
+    }
+    void userMove()
+    {
+        UserMove move = new UserMove();
+    }
+}
