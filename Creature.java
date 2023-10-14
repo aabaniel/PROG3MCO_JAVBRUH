5164f6f1c2520be35bf24a76072bc0f3bcafa9d6
Aaron Abaniel <140327394+aabaniel@users.noreply.github.com>
2023-10-14 22:23:11 +0800

    Update Creature.java
    Added the variables and setters

--- Creature.java
+++ Creature.java
@@ -1,3 +1,39 @@
 /*
 Implement a creature class here
 */
+public class creature {
+    private String cName;
+    private String cType;
+    private String cFam;
+    private Integer cHealth;
+    private Integer cEvLvl;
+    private Boolean IsAlive;
+    private Boolean IsEnemy;
+    private Boolean IsActive;
+
+    public void setcName(String cName) {
+        this.cName = cName;
+    }
+    public void setcType(String cType) {
+        this.cType = cType;
+    }
+    public void setcFam(String cFam) {
+        this.cFam = cFam;
+    }
+    public void setcHealth(Integer cHealth) {
+        this.cHealth = cHealth;
+    }
+    public void setcEvLvl(Integer cEvLvl) {
+        this.cEvLvl = cEvLvl;
+    }
+    public void setIsActive(Boolean isActive) {
+        IsActive = isActive;
+    }
+    public void setIsAlive(Boolean isAlive) {
+        IsAlive = isAlive;
+    }
+    public void setIsEnemy(Boolean isEnemy) {
+        IsEnemy = isEnemy;
+    }
+
+}
