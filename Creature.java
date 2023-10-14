7d7fd315f7ad54338761c69fb276acbbbd144b41
Aaron Abaniel <140327394+aabaniel@users.noreply.github.com>
2023-10-14 22:26:35 +0800

    Update Creature.java
    Added getters

--- Creature.java
+++ Creature.java
@@ -14,26 +14,50 @@ public class creature {
     public void setcName(String cName) {
         this.cName = cName;
     }
+    public String getcName() {
+        return cName;
+    }
     public void setcType(String cType) {
         this.cType = cType;
+    }
+    public String getcType() {
+        return cType;
     }
     public void setcFam(String cFam) {
         this.cFam = cFam;
     }
+    public String getcFam() {
+        return cFam;
+    }
     public void setcHealth(Integer cHealth) {
         this.cHealth = cHealth;
+    }
+    public Integer getcHealth() {
+        return cHealth;
     }
     public void setcEvLvl(Integer cEvLvl) {
         this.cEvLvl = cEvLvl;
     }
+    public Integer getcEvLvl() {
+        return cEvLvl;
+    }
     public void setIsActive(Boolean isActive) {
         IsActive = isActive;
+    }
+    public Boolean getIsActive() {
+        return IsActive;
     }
     public void setIsAlive(Boolean isAlive) {
         IsAlive = isAlive;
     }
+    public Boolean getIsAlive() {
+        return IsAlive;
+    }
     public void setIsEnemy(Boolean isEnemy) {
         IsEnemy = isEnemy;
+    }
+    public Boolean getIsEnemy() {
+        return IsEnemy;
     }
 
 }
