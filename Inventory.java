c79c7d045f487af39f15a9e8ab7248075e7bdc53
Disavowedd <128288879+Disavowedd@users.noreply.github.com>
2023-10-16 21:30:03 +0800

    Update Inventory.java, finished?

--- Inventory.java
+++ Inventory.java
@@ -1,12 +1,6 @@
-/*
-Create inventory class here
-  */
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class inventory {
     
-    ArrayList<Creature> creaturesList = new ArrayList<>(); 
+    ArrayList<Creature> creaturesList = new ArrayList<Creature>(); 
 
     private int inventorySelect;
     private String creatureInput; 
@@ -51,19 +45,39 @@ public class inventory {
             System.out.println("Invalid Selection, please only input 1-3");
             inventorySelect = scanner.nextInt();
         }
-        sc.close();
+        scanner.close();
     }
 
 
     private void selectActive()
     {
-    Scanner scanner = new Scanner(System.in);
+    Scanner scanner1 = new Scanner(System.in);
 
-    System.out.println("Which Creature Would you Like to Set as Active?\n");
-    System.out.println("Input the Creature name here:\n");
-    creatureInput = scanner.nextLine();
+    System.out.println("Which Creature Would you Like to Set as Active?");
+    System.out.println("Input the Creature name here:");
+    creatureInput = scanner1.nextLine();
 
+    boolean cFound = false; 
 
+    
+    while (!cFound) {
+        System.out.println("Enter the name of the creature: ");
+        creatureInput = scanner1.nextLine();
+
+        for (Creature creature : creaturesList) {
+            if (creatureInput.equalsIgnoreCase(creature.cName)) {
+                System.out.println("Creature found, Type: " + creature.cType + ", Health: " + creature.cEvLvl);
+                creature.IsActive = true; 
+                cFound = true;
+                break;
+            }
+        }
+
+        if (!cFound) {
+            System.out.println("Creature not found in the list. Please try again.");
+        }
+        }
+        scanner1.close();
     }
 
     private void showCreatures()
@@ -71,10 +85,20 @@ public class inventory {
      //For Displaying the Creatures: 
      //Creature newCreature = creaturesList.get(0);
      //System.out.println("Creature 1:" + firstCreature.getName() + firstCreature.getType() + firstCreature.getFamily() + firstCreature.getEL())
-    }
 
+    System.out.println("Showing All Creatures:");
+
+    for (Creature creature : creaturesList) {
+        if (creature.isActive) 
+        {
+            System.out.println("Currently Active: " + creature.cName + " - Type: " + creature.cType + ", Family: " + creature.cFam + ", EL: " + creature.cEvLvl);
+        } else
+         {
+            System.out.println(creature.cName + " - Type: " + creature.cType + ", Family: " + creature.cFam + ", EL: " + creature.cEvLvl);
+        }
+        }
+
+    }   
 
 }
-
-
 
